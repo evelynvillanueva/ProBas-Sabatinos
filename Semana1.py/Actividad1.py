8126928fdb5836638d0777a3bda4d3c344b2fc63
Evelyn Villanueva <eveale98@hotmail.com>
2025-02-03 19:04:07 -0600

    Semana1.py/Actividad10.py Semana1.py/Actividad2.py Semana1.py/Actividad3.py Semana1.py/Actividad4.py Semana1.py/Actividad5.py Semana1.py/Actividad6.py Semana1.py/Actividad7.py Semana1.py/Actividad8.py Semana1.py/Actividad9.py

---
+++ Semana1.py/Actividad1.py
@@ -0,0 +1,12 @@
+#Ejercicio1: Calculadora Básica. Descripción:
+#Escribe un programa que solicite al usuario dos números enteros y realice las operaciones aritméticas básicas (suma, resta, multiplicación, división, división de piso y residuo) entre ellos. Muestra los resultados en pantalla.
+
+a = int(input("Ingrese el número a: "))
+b = int(input("Ingrese el número b: "))
+
+print("suma: ", a+b)
+print("resta: ", a-b, b-a)
+print("multiplica: ", a*b, b*a)
+print("divide: ", a/b, b/a)
+print("div entera: ", a//b, b//a)
+print("modulo: ", a%b, b%a)
